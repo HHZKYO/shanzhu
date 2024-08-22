2d5428bc8e827e08e9db8448cdbffcb1a5f242e2
LHHZKYO <1692189552@qq.com>
2024-08-22 16:57:05 +0800

    封装Tags组件

--- src/shared/Tabs.tsx
+++ src/shared/Tabs.tsx
@@ -34,7 +34,8 @@ export const Tabs = defineComponent({
             </li>)}
         </ol>
         <div>
-          {tabs.find(item => item.props?.name === props.selected)}
+          {tabs.map(item => 
+            <div v-show={item.props?.name === props.selected}>{item}</div>)}
         </div>
       </div>
     }
