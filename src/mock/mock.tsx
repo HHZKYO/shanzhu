c3be952a07b66a4b6d45ab394d58c2ad71723692
LHHZKYO <1692189552@qq.com>
2024-08-22 18:36:24 +0800

    收集了四个字段 kind、tag_id、happen_at 和 amount

--- src/mock/mock.tsx
+++ src/mock/mock.tsx
@@ -5,22 +5,22 @@ type Mock = (config: AxiosRequestConfig) => [number, any]
 
 faker.setLocale('zh_CN');
 
-export const mockSession: Mock = (config) =>{
+export const mockSession: Mock = (config) => {
   return [200, {
     jwt: faker.random.word()
   }]
 }
 
+let id = 0
+const createId = () => {
+  id += 1
+  return id
+}
 export const mockTagIndex: Mock = (config) => {
   const { kind, page } = config.params
   const per_page = 25
   const count = 26
-  let id = 0
-  const createId = () => {
-    id += 1
-    return id
-  }
-  const createPager = (page = 1) => ({
+  const createPaper = (page = 1) => ({
     page, per_page, count
   })
   const createTag = (n = 1, attrs ?: any) =>
@@ -32,13 +32,13 @@ export const mockTagIndex: Mock = (config) => {
       ...attrs
     }))
     const createBody = (n = 1, attrs?:any) => ({
-      resources: createTag(n), pager: createPager(page)
+      resources: createTag(n), pager: createPaper(page)
     })
     if (kind === 'expenses' && (!page || page === 1)) {
       return [200, createBody(25)]
     } else if (kind === 'expenses' && page === 2) {
       return [200, createBody(1)]
-    } else if (kind === 'income' && (!page || page ===1)) {
+    } else if (kind === 'income' && (!page || page === 1)) {
       return [200, createBody(25)]
     } else {
       return [200, createBody(1)]
