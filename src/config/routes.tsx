136823b199f96ad196d6d6974313606bdbfa3084
LHHZKYO <1692189552@qq.com>
2024-09-11 15:11:20 +0800

    没有更新至vit3.0

--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -13,7 +13,6 @@ import { Third } from "../components/welcome/Third";
 import { ThirdActions } from "../components/welcome/ThirdActions";
 import { ItemPage } from "../views/ItemPage";
 import { SignInPage } from "../views/SignInPage";
-import { StartPage } from "../views/StartPage";
 import { StatisticsPage } from "../views/StatisticsPage";
 import { TagPage } from "../views/TagPage";
 import { Welcome } from "../views/Welcome";
